1db2b4373fba9a2a420f625e92413a04f59d8b99
suresh813 <shresthatula6677@gmail.com>
2026-04-27 13:33:44 +0100

    Upgrade service worker to version 2 with caching updates

--- service-Worker.js
+++ service-Worker.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = "calculator-v1";
+const CACHE_NAME = "calculator-v2";
 
 const urlsToCache = [
-"/",
-"index.html",
-"style.css",
-"script.js",
-"manifest.json"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
